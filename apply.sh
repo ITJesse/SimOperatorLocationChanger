a5452fe2c6f776b9e0ffa40678c3d1037560caa4
Jesse Zhu <jesse@itjesse.com>
2023-11-08 04:16:37 +0800

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,10 +1,10 @@
 PROPERTY='
-gsm.sim.operator.numeric=23402
-gsm.sim.operator.iso-country=gb
-gsm.sim.operator.alpha=O2
-gsm.operator.numeric=23402
-gsm.operator.iso-country=gb
-gsm.operator.alpha=O2
+gsm.sim.operator.numeric=44010
+gsm.sim.operator.iso-country=jp
+gsm.sim.operator.alpha=DoCoMo
+gsm.operator.numeric=44010
+gsm.operator.iso-country=jp
+gsm.operator.alpha=DoCoMo
 '
 
 for PROP in $PROPERTY; do
